5523513cbb59291c2964188a0f1cb3d24989b4d5
Jeffrey Lemoine <jeffmlife@gmail.com>
2025-07-10 20:39:04 -0700

    Modernize formula to use uv for virtual environment creation and package installation

--- Formula/streamlm.rb
+++ Formula/streamlm.rb
@@ -1,16 +1,22 @@
 class Streamlm < Formula
-  include Language::Python::Virtualenv
-
   desc "A command-line interface for interacting with various Large Language Models"
   homepage "https://github.com/jeffmylife/streamlm"
   url "https://files.pythonhosted.org/packages/03/eb/f5d1dcb643e85917afce5f84c44796e48f643ba7f910dffa8be7fd32b65a/streamlm-0.1.2.tar.gz"
   sha256 "86ee9eca39417cb076d9b2eb2e5c873a65161960923de0171ac4158ac787fd19"
   license "MIT"
 
+  depends_on "uv"
   depends_on "python3"
 
   def install
-    virtualenv_install_with_resources
+    # Use uv to create the virtual environment
+    system "uv", "venv", libexec, "--python", "python3"
+    
+    # Use uv to install the package (much faster than pip)
+    system "uv", "pip", "install", "--python", "#{libexec}/bin/python", buildpath
+    
+    # Create the executable link
+    bin.install_symlink libexec/"bin/lm"
   end
 
   test do
